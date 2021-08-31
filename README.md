bb0443932e632a7f92447bb25d4eca17db5c853b
Lena Dela <lena.delalic@hotmail.com>
2021-08-31 19:09:12 +0200

    added palindrome file and modified Readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # js-exam-01
 
 This is for homework: https://github.com/Himyo/DFS23C-js/blob/master/exam/scripts.md
+contents:
+- palindrome.js
---
+++ palindrome.js
@@ -0,0 +1,6 @@
+function palindrome(word) {
+    // TODO
+}
+console.log(palindrome('kayak')) // true
+console.log(palindrome('noname')) // false
+console.log(palindrome(1)) //Merci de mettre une string valable
